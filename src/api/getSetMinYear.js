5416141e9f0e246638ddd845819e3fa5c29e3894
elies-billotta <elies.billotta@edu.univ-eiffel.fr>
2024-03-12 00:13:06 +0100

    💄 Add page_size parameter to API request

--- src/api/getSetMinYear.js
+++ src/api/getSetMinYear.js
@@ -1,6 +1,6 @@
 const getSetMinYear = async (year, page) => {
     try{
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/sets/?key=${import.meta.env.VITE_API_KEY}&min_year=${year}`);
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/sets/?key=${import.meta.env.VITE_API_KEY}&min_year=${year}&page_size=300`);
         const data = await response.json();
         console.log(data)
         return data.results;
