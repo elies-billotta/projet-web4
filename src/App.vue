e6a295315ea706b8ce4853d07d9f09b8e28007a1
elies-billotta <elies.billotta@edu.univ-eiffel.fr>
2024-03-19 21:55:19 +0100

    🐛 Update localStorage settings for collection and sets

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,8 @@ export default {
     name: "App",
     created() {
         if (localStorage.getItem("collection") === null || localStorage.getItem("sets") === null){
-            localStorage.setItem("collection", JSON.stringify({ elements: [], filters : { search: "", themes: [], year: "2024", isRangeEnabled: true}}));
-            localStorage.setItem("sets", JSON.stringify({filters : { search: "", themes: [], year: "2024", isRangeEnabled: true}}));
+            localStorage.setItem("collection", JSON.stringify({ elements: [], filters : { search: "", themes: [], year: "2024", isRangeEnabled: false}}));
+            localStorage.setItem("sets", JSON.stringify({filters : { search: "", themes: [], year: "2024", isRangeEnabled: false}}));
         }
     },
 }
